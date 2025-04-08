5632e97b900a150b5d97b7a580f74922a8784a9c
JordanaVitoria <jordana.vitoria@estudante.iftm.edu.br>
2025-04-08 20:46:15 -0300

    criando projeto

---
+++ tds/src/test/java/org/iftm/tds/TdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iftm.tds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
