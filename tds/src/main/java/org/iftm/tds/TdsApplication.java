31451cecbd662a7298649cb7c89cd1ea7a4e3552
JordanaVitoria <jordana.vitoria@estudante.iftm.edu.br>
2025-04-29 19:35:19 -0300

    criando a classe animal e preenchendo data.sql de animal

--- tds/src/main/java/org/iftm/tds/TdsApplication.java
+++ tds/src/main/java/org/iftm/tds/TdsApplication.java
@@ -2,64 +2,66 @@ package org.iftm.tds;
 
 import java.util.List;
 
+import org.iftm.tds.entities.Animal;
 import org.iftm.tds.entities.Client;
+import org.iftm.tds.repositories.AnimalRepository;
 import org.iftm.tds.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*  modifiquei a classe incluindo a implementação da interface
-  commandLineRunner que permite inserir código a ser executado na
-  inicialização do servidor.
- */
-
 @SpringBootApplication
 public class TdsApplication implements CommandLineRunner {
-	//injeção de dependencia da classe ClientRepository.
-	//permite ao SpringBoot instanciar objetos dessa classe.
+
 	@Autowired
-	private ClientRepository repositorio;	
+	private ClientRepository repositorio;
+
+	@Autowired
+	private AnimalRepository animalRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TdsApplication.class, args);
 	}
-	@Override
- 	public void run(String... args) throws Exception {
- 		
- 
- 		//Instanciar um objeto da classe Client
- 		Client cliente = new Client();		
- 		cliente.setName("Ana");
- 		cliente.setCpf("000111222-22");
- 		cliente.setTelefone("3499999999");
- 		cliente.setQtdAnimals(3);
- 		cliente.setEndereco("Av Espanha");
- 		repositorio.save(cliente);
- 
-		//Client(Long id, String name, String cpf, Integer qtdAnimals, String telefone, String endereco)
- 		Client cliente2 = new Client(null, "João", "000111232-22", 3,"3499999989" , "Av Europa");
- 		repositorio.save(cliente2);
- 
- 		cliente2.setName("Maria");
- 		repositorio.save(cliente2);
- 
- 		//repositorio.deleteAll();
- 		//repositorio.deleteById(2L);
- 		List<Client> clientes = repositorio.findAll();
- 		System.out.println("Relatório:::::");
- 		for (int i = 0; i < clientes.size(); i++) {
- 			System.out.println(clientes.get(i).getName());
- 		}
- 
- 		// utilizando o método de busca por id, que é a chave primaria, ou seja,
- 		// retorna apenas um elemento
- 		// ele retorna um objeto Optional, o metodo get() retorna o Client
- 		Client busca = repositorio.findById(2L).get();
- 		System.out.println("Busca individual:");
- 		System.out.println(busca.getName());
- 	}
- 
- 
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		// CLIENTES
+		Client cliente = new Client(null, "Ana", "000111222-22", 3, "3499999999", "Av Espanha");
+		repositorio.save(cliente);
+
+		Client cliente2 = new Client(null, "João", "000111232-22", 3, "3499999989", "Av Europa");
+		repositorio.save(cliente2);
+
+		cliente2.setName("Maria");
+		repositorio.save(cliente2);
+
+		List<Client> clientes = repositorio.findAll();
+		System.out.println("Relatório de Clientes:::::");
+		for (Client c : clientes) {
+			System.out.println(c.getName());
+		}
+
+		Client busca = repositorio.findById(2L).get();
+		System.out.println("Busca individual (Client):");
+		System.out.println(busca.getName());
+
+		// ANIMAIS
+		Animal animal1 = new Animal(null, "Bidu", "Poodle", "Encaracolada", 8.5, "foto1.jpg");
+		Animal animal2 = new Animal(null, "Rex", "Pastor Alemão", "Curta", 25.0, "foto2.jpg");
+
+		animalRepository.save(animal1);
+		animalRepository.save(animal2);
+
+		List<Animal> animais = animalRepository.findAll();
+		System.out.println("Relatório de Animais:::::");
+		for (Animal a : animais) {
+			System.out.println(a.getNome() + " - " + a.getRaca());
+		}
+
+		Animal buscaAnimal = animalRepository.findById(1L).get();
+		System.out.println("Busca individual (Animal):");
+		System.out.println(buscaAnimal.getNome());
+	}
 }
--- tds/src/main/java/org/iftm/tds/entities/Animal.java
+++ tds/src/main/java/org/iftm/tds/entities/Animal.java
@@ -1,6 +1,5 @@
 package org.iftm.tds.entities;
 
-
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
@@ -9,74 +8,91 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 @Entity
-@Table(name="tb_client")
-public class Client implements Serializable {
+@Table(name = "tb_animal")
+public class Animal implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, length = 100)
+    @Column(name = "id", nullable = false)
     private Long id;
-    @Column(name = "client_name", nullable = false, length = 100)
-    private String name;
-    @Column(name = "cpf", nullable = false, length = 100)
-    private String cpf;
-    @Column(name = "quantidade_animais", nullable = false, length = 100)
-    private Integer qtdAnimals;
-    @Column(name = "telefone", nullable = false, length = 100)
-    private String telefone;
-    @Column(name = "endereco", nullable = false, length = 100)
-    private String endereco;
-    
-// Construtores, getters e setters
 
-public Client(Long id, String name, String cpf, Integer qtdAnimals, String telefone, String endereco) {
-    this.id = id;
-    this.name = name;
-    this.cpf = cpf;
-    this.endereco = endereco;
-    this.qtdAnimals = qtdAnimals;
-    this.telefone = telefone;
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
 
-}
-public Client(){
+    @Column(name = "raca", nullable = false, length = 100)
+    private String raca;
 
-}
-public Long getId() {
-    return id;
-}
-public void setId(Long id) {
-    this.id = id;
-}
-public String getName() {
-    return name;
-}
-public void setName(String name) {
-    this.name = name;
-}
-public String getCpf() {
-    return cpf;
-}
-public void setCpf(String cpf) {
-    this.cpf = cpf;
-}
-public String getEndereco() {
-    return endereco;
-}
-public void setEndereco(String endereco) {
-    this.endereco = endereco;
-}
-public Integer getQtdAnimals() {
-    return qtdAnimals;
-}
-public void setQtdAnimals(Integer qtdAnimals) {
-    this.qtdAnimals = qtdAnimals;
-}
-public String getTelefone() {
-    return telefone;
-}
-public void setTelefone(String telefone) {
-    this.telefone = telefone;
-}
-}
+    @Column(name = "pelagem", nullable = false, length = 100)
+    private String pelagem;
 
+    @Column(name = "peso", nullable = false)
+    private Double peso;
+
+    @Column(name = "foto", length = 255)
+    private String foto;
+
+    // Construtores
+    public Animal() {
+    }
+
+    public Animal(Long id, String nome, String raca, String pelagem, Double peso, String foto) {
+        this.id = id;
+        this.nome = nome;
+        this.raca = raca;
+        this.pelagem = pelagem;
+        this.peso = peso;
+        this.foto = foto;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPelagem() {
+        return pelagem;
+    }
+
+    public void setPelagem(String pelagem) {
+        this.pelagem = pelagem;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
